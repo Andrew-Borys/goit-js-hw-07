08811d02070b33df035b2b8edaa5696add8c3829
Andrew Borys <borysandrey1988@gmail.com>
2021-12-15 13:46:05 +0200

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMurkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMurkup);
+galleryRef.addEventListener("click", onImageToCreateModal);
+
+// const itemRef = document.querySelector(".gallery__link");
+
+// console.log(itemRef);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+  `;
+    })
+    .join("");
+}
+
+function onImageToCreateModal(e) {
+  e.preventDefault();
+
+  const selectedImage = e.target.dataset.source;
+  console.log(selectedImage);
+
+  const modal = basicLightbox.create(`
+      <img src="${selectedImage}" width="800" height="600">
+  `);
+
+  modal.show();
+
+  document.addEventListener("keydown", closeModal);
+  function closeModal(e) {
+    if (e.key === "Escape") {
+      modal.close();
+    }
+  }
+}
