373509d191cc752ebf2f10e0538ed5df826c9bff
Andrew Borys <borysandrey1988@gmail.com>
2021-12-15 16:42:49 +0200

    2 task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMurkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join("");
+}
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMurkup);
+
+const galleryMagic = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
